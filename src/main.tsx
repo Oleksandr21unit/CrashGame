3f7cfd3df706d382e3b156d696662f6612dbf706
Oleksandr <oleksandr.nazarenko@theunit.dev>
2024-05-16 17:52:02 +0300

    browser router basename

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App/App.tsx'
 import './style.css'
 import { BrowserRouter } from 'react-router-dom'
-// import { HashRouter as BrowserRouter } from 'react-router-dom'
 import { GameContextProvider } from './context/GameContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/CrashGame'>
     <GameContextProvider>
       {/* <React.StrictMode> */}
         <App />
